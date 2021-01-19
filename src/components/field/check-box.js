de4d985ab74bd53472053f0f766a46daa2312acc
Stuart Finley <finleysg@gmail.com>
2021-01-19 16:40:42 -0600

    use checkbox for registration

---
+++ src/components/field/check-box.js
@@ -0,0 +1,17 @@
+import { GiCheckMark } from "react-icons/gi"
+
+function CheckBox({ label, ...rest }) {
+  return (
+    <label className="my-checkbox">
+      <span className="checkbox__input">
+        <input type="checkbox" {...rest} />
+        <span className="checkbox__control">
+          <GiCheckMark />
+        </span>
+      </span>
+      {Boolean(label) && <span className="checkbox__label">{label}</span>}
+    </label>
+  )
+}
+
+export { CheckBox }
